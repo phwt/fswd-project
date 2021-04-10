eda449b1e7c1da0a68bffa11c68f2ac6ea48fa6c
phwt.smwt <phwt.smwt@gmail.com>
2021-04-10 13:29:09 +0700

    Setup API project

---
+++ api/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello from Express")
+})
+
+app.listen({ port: 5001 }, () => {
+  console.log(`Server ready at http://localhost:5001`)
+})
