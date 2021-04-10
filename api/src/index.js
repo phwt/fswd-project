09f2fe0ef1eb7f91264922a686535c88bb3a5776
phwt.smwt <phwt.smwt@gmail.com>
2021-04-10 13:34:39 +0700

    Config Prettier

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,13 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello from Express")
-})
+  res.status(200).send("Hello from Express");
+});
 
 app.listen({ port: 5001 }, () => {
-  console.log(`Server ready at http://localhost:5001`)
-})
+  console.log(`Server ready at http://localhost:5001`);
+});
