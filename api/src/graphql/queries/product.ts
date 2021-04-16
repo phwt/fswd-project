8af48802b25e1a9ddbf9629f101ab67f243757ef
phwt.smwt <phwt.smwt@gmail.com>
2021-04-16 20:11:06 +0700

    Update product.ts

--- api/src/graphql/queries/product.ts
+++ api/src/graphql/queries/product.ts
@@ -1,4 +1,7 @@
-import { ProductTC } from "../models";
+import { ProductTC, PromotionTC } from "../models";
 
 export const products = ProductTC.getResolver("findMany");
 export const productById = ProductTC.getResolver("findById");
+
+export const promotions = PromotionTC.getResolver("findMany");
+export const promotionById = PromotionTC.getResolver("findById");
