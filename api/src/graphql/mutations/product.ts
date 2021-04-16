282733273f552b9fa8cae4cd0d01cc95353ec781
phwt.smwt <phwt.smwt@gmail.com>
2021-04-16 18:37:41 +0700

    Add missing mutations

--- api/src/graphql/mutations/product.ts
+++ api/src/graphql/mutations/product.ts
@@ -2,3 +2,9 @@ import { ProductTC, PromotionTC } from "../models";
 
 export const createProduct = ProductTC.getResolver("createOne");
 export const createPromotion = PromotionTC.getResolver("createOne");
+
+export const updateProductById = ProductTC.getResolver("updateById");
+export const updatePromotionById = PromotionTC.getResolver("updateById");
+
+export const removeProductById = ProductTC.getResolver("removeById");
+export const removePromotionById = PromotionTC.getResolver("removeById");
