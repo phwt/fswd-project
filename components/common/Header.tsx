ba39c14b8e6ca84c5b46663abb2fd36b9033af12
tangtms <tang.tms@outlook.com>
2021-04-17 22:27:52 +0700

    Update Nav

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,15 +1,17 @@
 import { useSession } from "../../modules/SessionContext";
 import Link from "next/link";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Nav } from "react-bootstrap";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
   const { loading, user, logout: handleLogout } = useSession();
 
   return (
-    <div className="p-3 bg-dark">
+    <div className="p-3 bg-0 sticky">
       <Row>
+        <Col></Col>
         <Col>
-          <Link href="/">eCommerce</Link>
+          <h3 className="text-center raleway-6">FSWD</h3>
         </Col>
         <Col className="text-right">
           {loading && <span>Loading</span>}
@@ -31,8 +33,37 @@ const Header = () => {
               <Link href="/register">Register</Link>
             </>
           )}
+          {/* <FontAwesomeIcon icon={["fal", "cart"]} /> */}
         </Col>
       </Row>
+
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home" className="raleway-3s">
+            ALL PRODUCTS
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/" className="raleway-3s">
+            MEN
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/" className="raleway-3s">
+            WOMEN
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/" className="raleway-3s">
+            ACCESSORIES
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/" className="raleway-3s">
+            SALE
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 };
