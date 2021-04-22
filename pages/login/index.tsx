92999ee137f95cc8d4fcc6c37d46a9a36f5cba7b
phwt.smwt <phwt.smwt@gmail.com>
2021-04-22 10:38:52 +0700

    Auth page layout

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,12 +1,18 @@
 import { useCallback, useState } from "react";
-import { useSession } from "../../modules/SessionContext";
+import { useSession } from "@modules/SessionContext";
 import { useRouter } from "next/router";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import AuthContainer from "@components/AuthContainer";
 
 const LoginPage = () => {
   const router = useRouter();
   const { login } = useSession();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [result, setResult] = useState({
+    error: false,
+    detail: "",
+  });
 
   const handleUsernameChange = useCallback((e) => {
     setUsername(e.target.value);
@@ -20,7 +26,12 @@ const LoginPage = () => {
     async (e) => {
       e.preventDefault();
       if (await login(username, password)) await router.push("/");
-      else console.log("Invalid username or password"); // TODO: Show errors
+      else {
+        setResult({
+          error: true,
+          detail: "Invalid username or password",
+        });
+      }
     },
     [login, password, username]
   );
@@ -30,25 +41,54 @@ const LoginPage = () => {
   }, [router]);
 
   return (
-    <form className="LoginForm-form" onSubmit={handleLogin}>
-      <input
-        type="text"
-        value={username}
-        onChange={handleUsernameChange}
-        placeholder="Username"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={handlePasswordChange}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Login</button>
-      <button type="button" onClick={redirectToRegister}>
-        Register
-      </button>
+    <form onSubmit={handleLogin}>
+      <AuthContainer>
+        <Row>
+          <Col md={12} className="mb-3 text-center">
+            <h2>Login</h2>
+          </Col>
+          <Col md={12} className="mb-3">
+            <Form.Control
+              value={username}
+              onChange={handleUsernameChange}
+              placeholder="Username"
+              required
+              isInvalid={result.error}
+            />
+          </Col>
+
+          <Col md={12} className="mb-3">
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              placeholder="Password"
+              required
+              isInvalid={result.error}
+            />
+            <Form.Control.Feedback type="invalid">
+              {result.detail}
+            </Form.Control.Feedback>
+          </Col>
+
+          <Col md={6} className="mb-3">
+            <Button type="submit" block variant="success">
+              Login
+            </Button>
+          </Col>
+
+          <Col md={6} className="mb-3">
+            <Button
+              type="button"
+              onClick={redirectToRegister}
+              block
+              variant="outline-primary"
+            >
+              Register
+            </Button>
+          </Col>
+        </Row>
+      </AuthContainer>
     </form>
   );
 };
--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,11 +1,14 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useMutation } from "@apollo/client";
 import { CREATE_CUSTOMER_MUTATION } from "../../graphql/createCustomerMutation";
 import { useRouter } from "next/router";
+import AuthContainer from "@components/AuthContainer";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 const RegisterForm = () => {
   const router = useRouter();
 
+  const [useBilling, setUseBilling] = useState(false);
   const [newUser, setNewUser] = useState({
     username: "",
     email: "",
@@ -14,6 +17,15 @@ const RegisterForm = () => {
     phone: "",
     password: "",
   });
+
+  useEffect(() => {
+    if (useBilling) {
+      setNewUser({
+        ...newUser,
+        shippingAddress: newUser.billingAddress,
+      });
+    }
+  }, [useBilling]);
 
   const [createUser] = useMutation(CREATE_CUSTOMER_MUTATION);
 
@@ -38,55 +50,110 @@ const RegisterForm = () => {
   );
 
   return (
-    <form className="RegisterForm-form" onSubmit={handleRegister}>
-      <input
-        type="text"
-        name="email"
-        value={newUser.email}
-        onChange={handleInputChange}
-        placeholder="E-Mail"
-        required
-      />
-      <input
-        type="text"
-        name="username"
-        value={newUser.username}
-        onChange={handleInputChange}
-        placeholder="Username"
-        required
-      />
-      <input
-        type="password"
-        name="password"
-        value={newUser.password}
-        onChange={handleInputChange}
-        placeholder="Password"
-        required
-      />
-      <input
-        type="text"
-        name="phone"
-        value={newUser.phone}
-        onChange={handleInputChange}
-        placeholder="Phone"
-      />
-      <input
-        type="text"
-        name="billingAddress"
-        value={newUser.billingAddress}
-        onChange={handleInputChange}
-        placeholder="Billing Address"
-        required
-      />
-      <input
-        type="text"
-        name="shippingAddress"
-        value={newUser.shippingAddress}
-        onChange={handleInputChange}
-        placeholder="Shipping Address"
-        required
-      />
-      <button type="submit">Register</button>
+    <form onSubmit={handleRegister}>
+      <AuthContainer>
+        <Row>
+          <Col md={12} className="mb-3 text-center">
+            <h2>Register</h2>
+          </Col>
+
+          <Col md={12} className="mb-3">
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              value={newUser.email}
+              onChange={handleInputChange}
+              placeholder="Your email"
+              required
+            />
+          </Col>
+          <Col md={12} className="mb-3">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              name="username"
+              value={newUser.username}
+              onChange={handleInputChange}
+              placeholder="Your unique username"
+              required
+            />
+          </Col>
+          <Col md={12} className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value={newUser.password}
+              onChange={handleInputChange}
+              placeholder="Must be 6 or more characters long"
+              required
+            />
+          </Col>
+          <Col md={12} className="mb-3">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="text"
+              name="phone"
+              value={newUser.phone}
+              onChange={handleInputChange}
+              placeholder="Your mobile phone number"
+            />
+          </Col>
+          <Col md={12} className="mb-3">
+            <Form.Label>Billing Address</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              type="text"
+              name="billingAddress"
+              value={newUser.billingAddress}
+              onChange={handleInputChange}
+              placeholder="Billing Address"
+              required
+            />
+          </Col>
+          <Col md={12} className="mb-3">
+            <Form.Label>Shipping Address</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              type="text"
+              name="shippingAddress"
+              value={newUser.shippingAddress}
+              onChange={handleInputChange}
+              placeholder="Shipping Address"
+              required
+            />
+            <Form.Check
+              label="Same as billing address"
+              checked={useBilling}
+              onChange={({ target: { checked } }) => {
+                setUseBilling(checked);
+              }}
+            />
+          </Col>
+
+          <Col md={6} className="mb-3">
+            <Button type="submit" block variant="success">
+              Register
+            </Button>
+          </Col>
+
+          <Col md={6} className="mb-3">
+            <Button
+              type="button"
+              onClick={() => {
+                router.push("/login");
+              }}
+              block
+              variant="outline-primary"
+            >
+              Login
+            </Button>
+          </Col>
+        </Row>
+      </AuthContainer>
     </form>
   );
 };
