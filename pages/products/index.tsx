b830ecfab93580a0e2024e66eee823272565ce7a
tangtms <tang.tms@outlook.com>
2021-04-24 22:13:04 +0700

    Products and Promotions Page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,5 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
+import { CardColumns } from "react-bootstrap";
+import ProductCard from "../../components/common/ProductCard";
+
 const ProductsPage = () => {
-  return <p>Products Page</p>;
+  const query = gql`
+    query {
+      products(limit: 10, sort: _ID_DESC) {
+        _id
+        sku
+        name
+        detail
+        price
+        type
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(query);
+  return (
+    <>
+      {!loading && (
+        <>
+          <h3>
+            <b> ALL PRODUCTS</b>
+          </h3>
+          <br />
+          <CardColumns>
+            {data.products.slice(1).map((d) => {
+              return (
+                <ProductCard key={d.name.toString()} size="18" product={d} />
+              );
+            })}
+          </CardColumns>
+        </>
+      )}
+    </>
+  );
 };
 
 export default ProductsPage;
--- pages/promotions/index.tsx
+++ pages/promotions/index.tsx
@@ -1,5 +1,43 @@
+import { gql, useQuery } from "@apollo/client";
+import { CardColumns } from "react-bootstrap";
+import ProductCard from "../../components/common/ProductCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPercentage } from "@fortawesome/free-solid-svg-icons";
+
 const PromotionsPage = () => {
-  return <p>Promotions Page</p>;
+  const query = gql`
+    query {
+      promotions(limit: 10, sort: _ID_DESC) {
+        _id
+        sku
+        name
+        detail
+        price
+        type
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(query);
+  return (
+    <>
+      {!loading && (
+        <>
+          <h3>
+            <FontAwesomeIcon icon={faPercentage} />
+            <b> ALL PROMOTION</b>
+          </h3>
+          <br />
+          <CardColumns>
+            {data.promotions.slice(1).map((d) => {
+              return (
+                <ProductCard key={d.name.toString()} size="18" product={d} />
+              );
+            })}
+          </CardColumns>
+        </>
+      )}
+    </>
+  );
 };
 
 export default PromotionsPage;
