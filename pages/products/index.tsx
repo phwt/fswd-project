54c35ea2a88c3515057ea21ea15dcf1d7b617904
tangtms <tang.tms@outlook.com>
2021-04-27 02:58:42 +0700

    Update index.tsx

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,21 +1,71 @@
 import { gql, useQuery } from "@apollo/client";
-import { CardColumns } from "react-bootstrap";
+import { CardDeck, Pagination } from "react-bootstrap";
 import ProductCard from "../../components/common/ProductCard";
+import { useState, useEffect } from "react";
 
 const ProductsPage = () => {
-  const query = gql`
-    query {
-      products(limit: 10, sort: _ID_DESC) {
-        _id
-        sku
-        name
-        detail
-        price
-        type
+  const [currentPage, setCurrentPage] = useState(1);
+  const { loading, error, data } = useQuery(
+    gql`
+      query productPagination($currentPage: Int) {
+        productPagination(page: $currentPage, perPage: 5) {
+          items {
+            _id
+            sku
+            name
+            detail
+            price
+            type
+          }
+          count
+        }
       }
+    `,
+    {
+      variables: {
+        currentPage: currentPage,
+      },
     }
-  `;
-  const { loading, error, data } = useQuery(query);
+  );
+  // const query = gql`
+  //   query {
+  //     products(limit: 10, sort: _ID_DESC) {
+  //       _id
+  //       sku
+  //       name
+  //       detail
+  //       price
+  //       type
+  //     }
+  //   }
+  // `;
+  let items = [];
+  if (loading) {
+    for (let number = 1; number <= 10; number++) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          active={number === currentPage}
+          onClick={() => setCurrentPage(number)}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    }
+  } else {
+    for (let number = 1; number <= data.productPagination.count / 4; number++) {
+      items.push(
+        <Pagination.Item
+          key={number}
+          active={number === currentPage}
+          onClick={() => setCurrentPage(number)}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    }
+  }
+
   return (
     <>
       {!loading && (
@@ -24,15 +74,16 @@ const ProductsPage = () => {
             <b> ALL PRODUCTS</b>
           </h3>
           <br />
-          <CardColumns>
-            {data.products.slice(1).map((d) => {
+          <CardDeck>
+            {data.productPagination.items.map((d) => {
               return (
                 <ProductCard key={d.name.toString()} size="18" product={d} />
               );
             })}
-          </CardColumns>
+          </CardDeck>
         </>
       )}
+      <Pagination>{items}</Pagination>
     </>
   );
 };
