a535afaf9b6abc81d7074939da5e42cef7b627bd
phwt.smwt <phwt.smwt@gmail.com>
2021-05-02 01:27:49 +0700

    Add support for restriction based on role

--- frontend/modules/Auth.ts
+++ frontend/modules/Auth.ts
@@ -5,14 +5,25 @@ import { ME_QUERY } from "../graphql/meQuery";
  * Require authentication before accessing the page
  * Place inside getServerSideProps on page that need to be protected
  * @param context getServerSideProps context object
+ * @param roles Restrict page based on role
  */
-export const requireAuthentication = async (context) => {
+export const requireAuthentication = async (context, roles: string[] = []) => {
   const apolloClient = serverApollo(context);
   const {
     data: { me },
   } = await apolloClient.query({ query: ME_QUERY });
 
-  if (!me) {
+  const validAccess = () => {
+    if (me) {
+      if (roles.length) {
+        return roles.includes(me.role);
+      }
+      return true;
+    }
+    return false;
+  };
+
+  if (!validAccess()) {
     context.res.statusCode = 302;
     context.res.setHeader("Location", "/login");
     return false;
