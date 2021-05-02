d5c6f414e03f682354885dfbf1d859bce4975dc5
phwt.smwt <phwt.smwt@gmail.com>
2021-05-02 11:34:20 +0700

    Update Header.tsx

--- frontend/components/admin/Header.tsx
+++ frontend/components/admin/Header.tsx
@@ -1,42 +1,73 @@
 import { useSession } from "@modules/SessionContext";
-import { Col, Row, Nav } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
+import Link from "next/link";
 
 const Header = () => {
   const { loading, user, logout: handleLogout } = useSession();
+  const router = useRouter();
+
+  const topPath = useMemo(() => {
+    return router.pathname.split("/")[2];
+  }, [router.pathname]);
 
   return (
     <div className="p-4 bg-0 sticky-1">
       <div className="smartphone-menu-trigger" />
       <header className="avatar">
-        <Nav.Link href="/admin/">
-          <h2 className="font-weight-bold">Dashboard</h2>
-        </Nav.Link>
+        <Link href="/" passHref>
+          <Nav.Link>
+            <span>
+              <i className="fa fa-chevron-left mr-2" /> Back to store
+            </span>
+          </Nav.Link>
+        </Link>
+
+        <Link href="/admin/" passHref>
+          <Nav.Link>
+            <h2 className="font-weight-bold m-0">Dashboard</h2>
+          </Nav.Link>
+        </Link>
 
         <h6>
-        <Nav.Link>
-          {user && (
-            <>
-              Logged in as {user?.username}
-              <span className="mx-2">|</span>
-              <a href="#" onClick={handleLogout}>
-                Logout
-              </a>
-            </>
-          )}
+          <Nav.Link>
+            {user && (
+              <>
+                Logged in as {user?.username}
+                <span className="mx-2">|</span>
+                <a href="#" onClick={handleLogout}>
+                  Logout
+                </a>
+              </>
+            )}
           </Nav.Link>
         </h6>
+
+        <hr
+          className="m-0 my-2"
+          style={{
+            borderTop: "1px solid #FFFFFF44",
+          }}
+        />
       </header>
 
-      <Nav className="flex-column" activeKey="/home">
-        <Nav.Link href="/admin/products" className="raleway-3s">
-          Products
-        </Nav.Link>
-        <Nav.Link href="/admin/orders" className="raleway-3s">
-          Orders
-        </Nav.Link>
-        <Nav.Link href="/admin/promotions" className="raleway-3s">
-          Promotions
-        </Nav.Link>
+      <Nav className="flex-column" activeKey={topPath}>
+        <Link href="/admin/products" passHref>
+          <Nav.Link className="raleway-3s" eventKey="products">
+            Products
+          </Nav.Link>
+        </Link>
+        <Link href="/admin/orders" passHref>
+          <Nav.Link className="raleway-3s" eventKey="orders">
+            Orders
+          </Nav.Link>
+        </Link>
+        <Link href="/admin/promotions" passHref>
+          <Nav.Link className="raleway-3s" eventKey="promotions">
+            Promotions
+          </Nav.Link>
+        </Link>
       </Nav>
     </div>
   );
