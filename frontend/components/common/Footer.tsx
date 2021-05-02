e106c7ed690397acd3241d2df84c963e86dfafc8
tangtms <tang.tms@outlook.com>
2021-05-02 19:29:57 +0700

    Update Footer.tsx

--- frontend/components/common/Footer.tsx
+++ frontend/components/common/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <div className="p-2 bg-dark text-center">Copyright (c) 2021</div>;
+  return <div className="p-2 bg-0 text-center">Copyright (c) 2021</div>;
 };
 
 export default Footer;
